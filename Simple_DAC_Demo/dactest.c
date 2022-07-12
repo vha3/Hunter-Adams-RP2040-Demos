8fd3002af2b4219dca6d1d0971d31bf6b74468cd
Hunter Adams <hunteradams@dhcp-vl2042-7708.redrover.cornell.edu>
2022-07-12 15:53:42 -0400

    dac demo

---
+++ Simple_DAC_Demo/dactest.c
@@ -0,0 +1,95 @@
+/**
+ * V. Hunter Adams
+ * DDS of sine wave on MCP4822 DAC w/ ISR
+ * 
+ * Modified example code from Raspberry Pi
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+   GPIO 5 (pin 7) Chip select
+   GPIO 6 (pin 9) SCK/spi0_sclk
+   GPIO 7 (pin 10) MOSI/spi0_tx
+   3.3v (pin 36) -> VCC on DAC 
+   GND (pin 3)  -> GND on DAC 
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+//DDS parameters
+#define two32 4294967296.0 // 2^32 
+#define Fs 40000
+// the DDS units:
+volatile unsigned int phase_accum_main;
+volatile unsigned int phase_incr_main = (800.0*two32)/Fs ;//
+
+//DAC parameters
+// A-channel, 1x, active
+#define DAC_config_chan_A 0b0011000000000000
+// B-channel, 1x, active
+#define DAC_config_chan_B 0b1011000000000000
+
+//SPI configurations
+#define PIN_MISO 4
+#define PIN_CS   5
+#define PIN_SCK  6
+#define PIN_MOSI 7
+#define SPI_PORT spi0
+
+// DDS sine table
+#define sine_table_size 256
+volatile int sin_table[sine_table_size] ;
+
+// SPI data
+uint16_t DAC_data ; // output value
+
+// Timer ISR
+bool repeating_timer_callback(struct repeating_timer *t) {
+	// DDS phase and sine table lookup
+	phase_accum_main += phase_incr_main  ;
+    DAC_data = (DAC_config_chan_A | ((sin_table[phase_accum_main>>24] + 2048) & 0xffff))  ;
+
+    spi_write16_blocking(SPI_PORT, &DAC_data, 1) ;
+
+    return true;
+}
+
+int main() {
+    // Initialize stdio
+    stdio_init_all();
+    printf("Hello, DAC!\n");
+
+    // Initialize SPI channel (channel, baud rate set to 20MHz)
+    spi_init(SPI_PORT, 20000000) ;
+    // Format (channel, data bits per transfer, polarity, phase, order)
+    spi_set_format(SPI_PORT, 16, 0, 0, 0);
+
+    // Map SPI signals to GPIO ports
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS, GPIO_FUNC_SPI) ;
+
+    // === build the sine lookup table =======
+   	// scaled to produce values between 0 and 4096
+    int ii;
+    for (ii = 0; ii < sine_table_size; ii++){
+         sin_table[ii] = (int)(2047*sin((float)ii*6.283/(float)sine_table_size));
+    }
+
+    // Create a repeating timer that calls repeating_timer_callback.
+    // If the delay is > 0 then this is the delay between the previous callback ending and the next starting.
+    // If the delay is negative (see below) then the next call to the callback will be exactly x us after the
+    // start of the call to the last callback
+    struct repeating_timer timer;
+
+    // Negative delay so means we will call repeating_timer_callback, and call it again
+    // 25us (40kHz) later regardless of how long the callback took to execute
+    add_repeating_timer_us(-25, repeating_timer_callback, NULL, &timer);
+    while(1){
+    }
+    return 0;
+}
