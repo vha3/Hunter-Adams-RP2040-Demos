2efa34f46ac3c6ebf230b9cf9b26cc70f87a44cf
Hunter Adams <hunteradams@dhcp-vl2041-18182.eduroam.cornell.edu>
2025-08-04 15:16:48 -0400

    updated VGA fonts and colors demo

--- VGA_Graphics/VGA_Fonts_and_Colors/vga16_graphics_demo.c
+++ VGA_Graphics/VGA_Fonts_and_Colors/vga16_graphics_demo.c
@@ -2,43 +2,41 @@
  * Hunter Adams (vha3@cornell.edu)
  * 
  * HARDWARE CONNECTIONS
- *  - GPIO 16 ---> VGA Hsync
- *  - GPIO 17 ---> VGA Vsync
- *  - GPIO 18 ---> 470 ohm resistor ---> VGA Green 
- *  - GPIO 19 ---> 330 ohm resistor ---> VGA Green
- *  - GPIO 20 ---> 330 ohm resistor ---> VGA Blue
- *  - GPIO 21 ---> 330 ohm resistor ---> VGA Red
- *  - RP2040 GND ---> VGA GND
- * 
+ *  
+- GPIO 16 ---> VGA Hsync
+- GPIO 17 ---> VGA Vsync
+- GPIO 18 ---> VGA Green lo-bit --> 470 ohm resistor --> VGA_Green
+- GPIO 19 ---> VGA Green hi_bit --> 330 ohm resistor --> VGA_Green
+- GPIO 20 ---> 330 ohm resistor ---> VGA-Blue
+- GPIO 21 ---> 330 ohm resistor ---> VGA-Red
+- RP2040 GND ---> VGA-GND
+ *
  * RESOURCES USED
  *  - PIO state machines 0, 1, and 2 on PIO instance 0
- *  - DMA channels obtained by claim mechanism
+ *  - DMA channels 0, 1, 2, and 3
  *  - 153.6 kBytes of RAM (for pixel color data)
  *
- * Protothreads v1.1.3
+ * Protothreads v1.4
  * Threads:
  * core 0:
  * Graphics demo
  * blink LED25 
  * core 1:
- * Toggle gpio 4 
  * Serial i/o 
  */
 // ==========================================
 // === VGA graphics library
 // ==========================================
-#include "vga16_graphics.h"
+#include "vga16_graphics_v2.h"
 #include <stdio.h>
 #include <stdlib.h>
 // #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/dma.h"
-// // Our assembled programs:
-// // Each gets the name <pio_filename.pio.h>
-// #include "hsync.pio.h"
-// #include "vsync.pio.h"
-// #include "rgb.pio.h"
+//
+#include "hardware/vreg.h"
+#include "hardware/clocks.h"
 
 // ==========================================
 // === protothreads globals
@@ -48,10 +46,12 @@
 #include "pico/multicore.h"
 #include "string.h"
 // protothreads header
-#include "pt_cornell_rp2040_v1_3.h"
-
-char user_string[40] = "Type up to 40 characters" ;
-int new_str = 1;
+#include "pt_cornell_rp2040_v1_4.h"
+//
+// restart graphics flag
+bool restart_graphics = true ;
+// shared variable for erase color
+int bkgnd_color = 0 ;
 
 // ==================================================
 // === graphics demo -- RUNNING on core 0
@@ -61,25 +61,22 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     // the protothreads interval timer
     PT_INTERVAL_INIT() ;
 
-    // circle radii
-    static short circle_x = 0 ;
+    static uint64_t draw_time ;
+    static int time ;
 
-    // color chooser
-    static char color_index = 0 ;
-
-    // position of the disc primitive
-    static short disc_x = 0 ;
-    // position of the box primitive
-    static short box_x = 0 ;
-    // position of vertical line primitive
-    static short Vline_x = 350;
-    // position of horizontal line primitive
-    static short Hline_y = 250;
-
+    // position of the display primitivea
+    static short filled_circle_x = 0, filled_circle_color = 3;
+    static short circle_x = 100, circle_color = 11 ; 
+    static short rect_x = 300 , rect_color = 7;
+    static short filled_rect_x = 200,  filled_rect_color = 9;
+    static short round_rect_x = 400 , round_rect_color = 7;
+    static short filled_round_rect_x = 500,  filled_round_rect_color = 12;
+    static short right_side = 638 ;
+    //
     // Draw some filled rectangles
     fillRect(64, 0, 176, 50, BLUE); // blue box
-    //fillRect(250, 0, 176, 50, DARK_ORANGE); // red box
-    fillRect(435, 0, 176, 50, LIGHT_BLUE); // green box
+    fillRect(250, 0, 176, 50, YELLOW); // red box
+    fillRect(435, 0, 176, 50, GREEN); // green box
 
     // Write some text
     setTextColor(WHITE) ;
@@ -87,36 +84,31 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     setTextSize(1) ;
     writeString("Raspberry Pi Pico") ;
     setCursor(65, 10) ;
-    writeString("Font demo") ;
+    writeString("Graphics primitives demo") ;
     setCursor(65, 20) ;
-    writeString("Hunter Adams") ;
+    writeString("Hunter Adams vha3@cornell.edu") ;
     setCursor(65, 30) ;
-    writeString("Bruce Land") ;
-    setCursor(65, 40) ;
-    writeString("ece4760 Cornell") ;
+    writeString("Bruce Land brl4@cornell.edu") ;
     //
-    setCursor(445, 10) ;
+    setCursor(255, 10) ;
+    setTextColor(BLACK) ;
+    writeStringBig("Test vga_graphics_v2") ;
+    //
+    setCursor(438, 10) ;
     setTextColor(BLACK) ;
     setTextSize(1) ;
-    writeString("Protothreads rp2040 v1.3") ;
-    setCursor(445, 20) ;
-    writeString("VGA 4-bit color") ;
-    setCursor(445, 30) ;
-    writeString("Tested on pico and picoW") ;
-    //
-    setCursor(270, 20) ;
-    setTextColor2(WHITE, BLACK) ;
-    writeStringBig("VGA437 Font Test") ;
-    setCursor(270, 37) ;
-    setTextColor2(WHITE, BLACK) ;
-    writeStringBold("GLCD BOLD Font Test") ;
-    
+    writeString("Protothreads rp2040/2350 1.4") ;
+    setCursor(438, 20) ;
+    writeString("CPU clock 150 Mhz") ;
+    setCursor(438, 30) ;
+    writeString("vga16_v2 driver") ;
+    setTextColor(WHITE) ;
 
     // Setup a 1Hz timer
     //static struct repeating_timer timer;
-    //add_repeating_timer_ms(-1000, repeating_timer_callback, NULL, &timer);
+    ////add_repeating_timer_ms(-1000, repeating_timer_callback, NULL, &timer);
 
-     static char video_buffer[32];
+     char video_buffer[64];
     setTextColor2(WHITE, BLACK) ;
     setTextSize(1) ;
     for (int i=0; i<4; i++) {
@@ -163,13 +155,13 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     writeString("LIGHT_PINK") ;
     setCursor(3*70+20, 200+3*70) ; 
     writeString("WHITE") ;
-    
+
     // small font
     int line_spacing = 8;
     int line_base = 150 ;
     setCursor(330, line_base) ; 
     setTextColor2(GREEN, BLACK);
-    writeString("Testing 5x7 font: size=1");
+    writeString("GLCD 5x7 font: size=1");
 
     setCursor(330, line_base+line_spacing) ; 
     setTextColor2(RED, BLACK);
@@ -192,7 +184,7 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     line_base = 200 ;
     setCursor(330, line_base) ; 
     setTextColor2(GREEN, BLACK);
-    writeStringBold("Testing 5x7 BOLD font: size=1");
+    writeStringBold("GLCD 5x7 BOLD font: size=1");
 
     setCursor(330, line_base+line_spacing) ; 
     setTextColor2(RED, BLACK);
@@ -216,23 +208,23 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     setTextSize(2);
     setCursor(300, line_base) ; 
     setTextColor2(GREEN, BLACK);
-    writeStringBold("5x7 BOLD font: size=2");
+    writeString("GLCD 5x7 font: size=2");
 
     setCursor(300, line_base+line_spacing) ; 
     setTextColor2(RED, BLACK);
-    writeStringBold("1234567890~!@#$%^&*()");
+    writeString("1234567890~!@#$%^&*()");
 
     setCursor(300, line_base+line_spacing*2) ; 
     setTextColor2(ORANGE, BLACK);
-    writeStringBold("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    writeString("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     setTextColor2(LIGHT_BLUE, BLACK);
     setCursor(300, line_base+line_spacing*3) ; 
-    writeStringBold("abcdefghijklmnopqrstuvwxyz");
+    writeString("abcdefghijklmnopqrstuvwxyz");
 
     setCursor(300, line_base+line_spacing*4) ; 
     setTextColor2(PINK, BLACK);
-    writeStringBold("`-+=;:,./?|");
+    writeString("`-+=;:,./?|");
 
     // big font
     line_spacing = 15;
@@ -240,7 +232,7 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     setTextSize(1);
     setCursor(330, line_base) ; 
     setTextColor2(GREEN, BLACK);
-    writeStringBig("Testing 8x16 font: \x13 only one size \x13");
+    writeStringBig("VGA437 8x16 font: \x13 only one size \x13");
 
     setCursor(330, line_base+line_spacing) ; 
     setTextColor2(RED, BLACK);
@@ -257,83 +249,153 @@ static PT_THREAD (protothread_graphics(struct pt *pt)) {
     setCursor(330, line_base+line_spacing*4) ; 
     setTextColor2(PINK, BLACK);
     writeStringBig("`-+=;:,./?|");
-
-    char test_str[32] ={1,32,2,32,3,32,4,32,5,32,6,32,7,32,8,32, 
-                        9,32,10,32,11,32,12,32,13,32,14,32,15, 0} ;
-    setCursor(330, line_base+line_spacing*5) ; 
-    setTextColor2(GREEN, BLACK);
-    writeStringBig(test_str);
-
-    char test_str_2[32] ={16,32,17,32,18,32,19,32,20,32,21,32,22,32,23,
-                      32,24,32,25,32,26,27,32,28,32,29,32,30,32,31,0} ;
-    setCursor(330, line_base+line_spacing*6) ; 
-    setTextColor2(GREEN, BLACK);
-    writeStringBig(test_str_2);
-
-    char test_str_3[32] ={8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7, 0} ;
-    char test_str_4[32] ={32,9,10,9,10,9,10,9,10,9,10,9,10,9,10,9,10, 0} ;
-    setCursor(330, line_base+line_spacing*7) ; 
-    setTextColor2(GREEN, BLACK);
-    writeStringBig(test_str_3);
-    setCursor(330, line_base+line_spacing*8) ;
-    writeStringBig(test_str_4);
-
-
-    ///setCursor(100, 320) ; 
-    //setTextColor(GREEN);
-    //writeStringBig("testing transparent");
-
-    static char white_text[40] = "White text test ABC abc 123";
-    static char black_text[40] = "Black text test ABC abc 123";
-    static char vary_text[40] = "Random color text test ABC abc 123";
+    
+    drawHLine(75, 430, 50, WHITE );
+    setCursor(5, 430) ; 
+    setTextColor2(WHITE, BLACK);
+    writeStringBold("drawHLine");
+    //
+    drawLine(75, 445, 120, 452, WHITE);
+    setCursor(5, 440) ; 
+    setTextColor2(WHITE, BLACK);
+    writeStringBold("drawLine");
+    //
+    drawVLine(75, 450, 25, WHITE );
+    setCursor(5, 450) ; 
+    setTextColor2(WHITE, BLACK);
+    writeStringBold("drawVLine");
+    //
+    crosshair(220, 454, GREEN);
+    drawPixel(222, 458, GREEN) ;
+    setCursor(150, 450) ; 
+    setTextColor2(WHITE, BLACK);
+    writeStringBold("drawPixel");
+    //
+    setCursor(340, 430) ; 
+    setTextColor2(WHITE, BLACK);
+    writeStringBold("Serial cmd: (e)rase to color (0-15)");
+    setCursor(340, 440) ; 
+    setTextColor2(WHITE, BLACK);
+    writeStringBold("Serial cmd: (r)estart graphics");
+ 
 
     while(true) {
 
-        // Modify the color 
-        if (color_index ++ == 15) color_index = 0 ;
-
-        fillRect(50, 60, 550, 60, color_index);
-
-        setTextColor2(BLACK, color_index );
-        setCursor(55, 65) ; 
-        writeStringBig(black_text);
-        setCursor(55, 85) ; 
-        writeString(black_text);
-
-        setTextColor2(WHITE, color_index );
-        setCursor(320, 65) ; 
-        writeStringBig(white_text);
-        setCursor(320, 85) ; 
-        writeString(white_text);
-        //setTextColor(WHITE);
-        //setCursor(321, 85) ; 
-        //writeString(white_text);
-
-        static  char text_color ;
-        text_color = rand()&0x0f;
-        setTextColor2(text_color, color_index );
-        setCursor(75, 100) ; 
-        writeStringBig(vary_text);
-
-        sprintf(video_buffer, "rect:%02d  text:%02d", color_index, text_color) ;
-        setCursor(400, 100) ; 
-        writeStringBig (video_buffer);
-
-        // user string
-        if (new_str){
-          new_str = false ;
-          // erase
-          setCursor(40, 450) ;
-          setTextColor2(WHITE, BLACK ); 
-          writeStringBig ("                                       ");
-          //
-          setCursor(40, 450) ;
-          setTextColor2(text_color, BLACK ); 
-          writeStringBig (user_string);
+        // restart logic
+        if (restart_graphics){
+            // reset the flag
+            restart_graphics = false ;       
+            // restarts the current thread at PT_BEGIN(pt);
+            // as if it has not executed before
+            PT_RESTART(pt) ;
         }
-  
-        // A brief nap
-        PT_YIELD_usec(3000000) ;
+        // frame number
+        time++;
+        // clear the row of shapes
+        // during the vertical interval
+        // this syncs the thread to the frame rate
+        while(gpio_get(VSYNC)){};
+        // DOES NOT CHECK for an edge, so very fast drawing
+        // MAY need a 1 mSec yield at the end
+        //PT_YIELD_UNTIL(pt, !gpio_get(VSYNC));
+       // draw_time = PT_GET_TIME_usec();
+        // clear takes 200 uSec
+        clearRect(0, 50, 640, 150, (short)bkgnd_color);
+
+        draw_time = PT_GET_TIME_usec();
+
+        // A row of graphics primitives
+        fillCircle(filled_circle_x, 100, 45, filled_circle_color);
+        setCursor(filled_circle_x-35, 100) ; 
+        setTextColor2(WHITE, BLACK);
+        writeStringBold("fillCircle");
+
+        //
+        drawCircle(circle_x, 100, 45, circle_color);
+        drawCircle(circle_x+1, 100, 45, circle_color);
+        drawCircle(circle_x, 100, 44, circle_color);
+        setCursor(circle_x-35, 100) ; 
+        setTextColor2(WHITE, BLACK);
+        writeStringBold("drawCircle");
+        //
+        fillRect(filled_rect_x, 52, 85, 85, filled_rect_color) ;
+        setCursor(filled_rect_x+5, 100) ; 
+        setTextColor2(WHITE, BLACK);
+        writeStringBold("fillRect");
+        //
+        drawRect(rect_x, 52, 85, 85, rect_color) ;
+        setCursor(rect_x+5, 100) ; 
+        setTextColor2(WHITE, BLACK);
+        writeStringBold("drawRect");
+        //
+        //
+        fillRoundRect(filled_round_rect_x, 60, 86, 60, 5, filled_round_rect_color) ;
+        setCursor(filled_round_rect_x+1, 100) ; 
+        setTextColor2(WHITE, BLACK);
+        writeStringBold("fillRoundRect");
+        //
+        //drawRoundRect(round_rect_x+1, 53, 85, 83, 10, round_rect_color) ;
+        drawRoundRect(round_rect_x, 70, 86, 60, 5, round_rect_color) ;
+        setCursor(round_rect_x+1, 100) ; 
+        setTextColor2(WHITE, BLACK);
+        writeStringBold("drawRoundRect");
+       // drawRoundRect(round_rect_x+2, 54, 81, 83, 10, round_rect_color) ;
+        // upate position
+        filled_circle_x += 2 ;
+        circle_x += 3 ;
+        filled_rect_x += 1 ;
+        rect_x += (time & 0x1)==0 ;
+        filled_round_rect_x += (time & 0x1)==0 ;
+        round_rect_x += 1 ;
+
+        // range check on position
+        if(filled_circle_x > right_side) {
+          filled_circle_x = 0;
+          filled_circle_color = rand() & 0xf ;
+        }
+        if(circle_x > right_side) {
+          circle_x = 0;
+          circle_color = rand() & 0xf ;
+        }
+        if(filled_rect_x > right_side) {
+          filled_rect_x = 0;
+          filled_rect_color = rand() & 0xf ;
+        }
+        if(rect_x > right_side) {
+          rect_x = 0;
+          rect_color = rand() & 0xf ;
+        }
+        if(filled_round_rect_x > right_side) {
+          filled_round_rect_x = 0;
+          filled_round_rect_color = rand() & 0xf ;
+        }
+        if(round_rect_x > right_side) {
+          round_rect_x = 0;
+          round_rect_color = rand() & 0xf ;
+        }
+        
+        // read back a pixel and show it
+        uint8_t pix_color = readPixel(320,100) ;
+        // now mark the pixel
+        crosshair(320, 100, WHITE);
+        // and draw a string
+        setCursor(320, 130) ;
+        if(pix_color>5)  setTextColor2(BLACK, pix_color) ;
+        else  setTextColor2(WHITE, pix_color) ;
+        writeStringBig("Pixel (320,100) color") ;
+        //
+        drawVLine(320,100,30, WHITE) ;
+        drawVLine(321,100,30, BLACK) ;
+        drawVLine(319,100,30, BLACK) ;
+
+        sprintf(video_buffer, "Draw Time %4.1f mSec", (float)(PT_GET_TIME_usec()-draw_time)/1000);
+        setTextColor2(WHITE, BLACK) ;
+        setCursor(260, 450);
+        writeStringBig(video_buffer) ;
+
+        // Thread is paced by the Vsync edge
+        PT_YIELD_usec(20000) ;
+        //PT_YIELD(pt);
    }
    PT_END(pt);
 } // graphics thread
@@ -368,6 +430,7 @@ static PT_THREAD (protothread_toggle25(struct pt *pt))
   PT_END(pt);
 } // blink thread
 
+
 // ==================================================
 // === user's serial input thread on core 1
 // ==================================================
@@ -376,19 +439,30 @@ static PT_THREAD (protothread_toggle25(struct pt *pt))
 static PT_THREAD (protothread_serial(struct pt *pt))
 {
     PT_BEGIN(pt);
-      static int test_in1, test_in2, sum ;
-      //
+      static uint64_t e_time ;
+
       while(1) {
         // print prompt
-        sprintf(pt_serial_out_buffer, "input string <40 char: ");
+        sprintf(pt_serial_out_buffer, "erase/restart: ");
         // spawn a thread to do the non-blocking write
         serial_write ;
 
         // spawn a thread to do the non-blocking serial read
-         serial_read ;
+        serial_read ;
         // convert input string to number
-        strcpy(user_string, pt_serial_in_buffer) ;
-        new_str = true ;
+        //sscanf(pt_serial_in_buffer,"%d %d", &test_in1, &test_in2) ;
+        short color;
+        if(pt_serial_in_buffer[0]=='e'){
+          sscanf(pt_serial_in_buffer+1,"%d\r", &bkgnd_color) ;
+          // wipe the whole frame
+          e_time = PT_GET_TIME_usec();
+          clearLowFrame(0, (short)bkgnd_color) ;
+          printf( "Erase Time %lld uSec\n\r", (PT_GET_TIME_usec() - e_time));
+        }
+
+        if(pt_serial_in_buffer[0]=='r'){
+          restart_graphics = true;
+        }
 
         // NEVER exit while
       } // END WHILE(1)
@@ -402,7 +476,6 @@ void core1_main(){
   //
   //  === add threads  ====================
   // for core 1
-  //pt_add_thread(protothread_toggle_gpio4) ;
   pt_add_thread(protothread_serial) ;
   //
   // === initalize the scheduler ==========
@@ -416,15 +489,17 @@ void core1_main(){
 // ========================================
 int main(){
   // set the clock
-  //set_sys_clock_khz(250000, true); // 171us
+  set_sys_clock_khz(150000, true);
+
   // start the serial i/o
   stdio_init_all() ;
   // announce the threader version on system reset
-  printf("\n\rProtothreads RP2040 v1.3 two-core, priority\n\r");
+  printf("\n\rProtothreads RP2040/2350 v1.4 \n\r");
 
   // Initialize the VGA screen
   initVGA() ;
-     
+  //printf("video assumes cpu clock=%d MHz", rgb_vid_freq);
+
   // start core 1 threads
   multicore_reset_core1();
   multicore_launch_core1(&core1_main);
